874fcbc1386cc4f7ca853fb5e1500a688db5d03e
qiqin <qiqin@primeton.com>
2023-06-01 16:14:14 +0800

    add: 新增test模块

---
+++ test/src/main/java/com/qq/test/Test.java
@@ -0,0 +1,44 @@
+package com.qq.test;
+
+import cn.hutool.core.date.*;
+
+import java.io.File;
+import java.net.*;
+import java.util.*;
+import java.util.function.Function;
+
+/**
+ * 算法测试类 //TODO <br><br>
+ *
+ * <p>
+ * Class detailed description, effection, usage,etc. //TODO
+ * </p >
+ * <br>
+ * author: qiqin 2023/5/29 11:07
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
