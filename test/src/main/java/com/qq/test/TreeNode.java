e7bb348ca379c8893bbe263e018d6b8e089b0336
qiqin <qiqin@primeton.com>
2023-12-26 17:07:57 +0800

    update: 修改test模块

---
+++ test/src/main/java/com/qq/test/TreeNode.java
@@ -0,0 +1,25 @@
+package com.qq.test;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int hashCode() {
+        return val;
+    }
+}
---
+++ test/src/test/java/com/qq/test/MyTest.java
@@ -0,0 +1,10 @@
+package com.qq.test;
+
+import org.junit.Test;
+
+public class MyTest {
+    @Test
+    public void test() {
+
+    }
+}
