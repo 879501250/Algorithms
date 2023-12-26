8e164de0cabb326ccbf72a20f5f7d439fbcf03e4
qiqin <qiqin@primeton.com>
2023-12-26 17:03:21 +0800

    update: 修改test模块

--- test/src/main/java/com/qq/test/Test.java
+++ test/src/main/java/com/qq/test/Test.java
@@ -1,14 +1,5 @@
 package com.qq.test;
 
-import com.qq.utils.JarScanner;
-
-import java.io.*;
-import java.lang.reflect.Array;
-import java.net.*;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.logging.Level;
-
 /**
  * 算法测试类 //TODO <br><br>
  *
@@ -21,50 +12,8 @@ import java.util.logging.Level;
 public class Test {
 
     public static void main(String[] args) throws Exception {
-        
+
     }
 
-
-}
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-
-    @Override
-    public int hashCode() {
-        return val;
-    }
-}
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
 }
 
