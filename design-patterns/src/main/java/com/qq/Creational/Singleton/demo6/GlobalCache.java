9ec0fd4b3f07d703c6d4426292899c6fd1c2ca86
qiqin <qiqin@primeton.com>
2024-02-22 17:45:43 +0800

    update: 添加双重校验单例模式的经典实现为什么要加 volatile 的解释

--- design-patterns/src/main/java/com/qq/Creational/Singleton/demo6/GlobalCache.java
+++ design-patterns/src/main/java/com/qq/Creational/Singleton/demo6/GlobalCache.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
- * 双重锁校验保证线程安全
+ * 双重锁校验保证线程安全，实例要使用 volatile 修饰，保证线程安全
  */
 public class GlobalCache {
     private static volatile Map<String, Object> instance;
